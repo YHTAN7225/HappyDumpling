708364eda14815bb6d7f7bdc905664546e6d9369
YHTAN7225 <yihow_tan@hotmail.com>
2021-12-19 19:01:11 +0800

    add storefront UI header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,78 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand">{{ NavBarTitle }}</a>
-        <button class="navbar-toggler" type="button" @click="toggleNavBar=!toggleNavBar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div :class="toggleNavBar ? 'collapse navbar-collapse' : 'navbar-collapse'" id="navbarNav">
-            <ul class="navbar-nav">
-            <li class="nav-item" v-for="item in NavBarComponent" :key="item.name">
-                <a class="nav-link" @click="$router.push({name : item.routerName})">{{ item.name }}</a>
-            </li>
-        
-            </ul>
-        </div>
-    </nav>
+    <div>
+        <SfHeader
+            :logo="require('@/assets/images/HappyDumplingLogoLong.png')"
+            :logoHeight="50"
+            :logoWidth="150"
+            title="Storefront UI"
+            cartIcon="empty_cart"
+            accountIcon="profile"
+            searchPlaceholder="Search for items"
+            @enter:search="search"
+            @change:search="changeSearch"
+            @click:cart="redirect('cart')"
+            @click:account="redirect('account')"
+        >
+            <template v-slot:aside>
+                <div v-if="showAside" @click="showHeaderMenu = !showHeaderMenu">
+                    <SfIcon
+                        icon="tiles"
+                        size="lg"
+                        color="black"
+                        viewBox="0 0 24 24"
+                        :coverage="1"
+                    />
+                </div>
+            </template>
+            <template v-slot:navigation>
+                <div>
+                    <nav class="sf-breadcrumbs" aria-label="breadcrumb">
+                        <ol class="sf-breadcrumbs__list">
+                            <li :class="'sf-breadcrumbs__list-item' + (selectedNav == item.routerName ? ' text-selected :' : '')" 
+                            aria-current="page" 
+                            v-for="item in NavBarComponent" 
+                            :key="item.name" 
+                            @click="selectedNav = item.routerName; redirect(item.routerName)">
+                                {{item.name}}
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </template>
+        </SfHeader>
+        <SfAccordion open="" :multiple="false" transition="" v-if="showHeaderMenu">
+            <SfList>
+                <SfListItem 
+                :class="(selectedNav == item.routerName) ? 'mobile_header_list_selected' : 'mobile_header_list'"
+                v-for="item in NavBarComponent" 
+                :key="item.name">
+                    <SfMenuItem :label="item.name" @click="selectedNav = item.routerName; redirect(item.routerName); showHeaderMenu=false"/>
+                </SfListItem>
+            </SfList>
+        </SfAccordion>
+    </div>
+    
+    
+
 </template>
 
 <script>
+import {SfHeader} from "@storefront-ui/vue";
+import {SfAccordion} from "@storefront-ui/vue";
+import {SfIcon} from "@storefront-ui/vue";
+import {SfList} from "@storefront-ui/vue";
+import {SfMenuItem} from "@storefront-ui/vue";
+
     export default {
         name : 'Header',
         data(){
             return {
                 toggleNavBar : true,
+                searchValue : "default",
+                selectedNav : "",
+                windowWidth : 0,
+                showAside : false,
+                showHeaderMenu : false,
             }
         },
         props : {
@@ -46,12 +98,50 @@
                 type : String,
                 require : false,
                 default : "Navbar"
-            }
+            },
+        },
+        components : {
+            SfHeader,
+            SfAccordion,
+            SfIcon,
+            SfList,
+            SfMenuItem
+        },
+        watch : {
+            windowWidth : function(oldVal){            
+                this.showAside = (oldVal < 1024) ? true : false;
+            },
         },
         methods : {
+            test : function(){
+                console.log("asdasd");
+            },
             redirect : function(routerName){
                 this.$router.push({name : routerName});
+            },
+            changeSearch : function(searchResult){
+                this.searchValue = searchResult;
+            },
+            search : function(){
+                //search
+            },
+            goToCart : function(input){
+                //go to cart page
+            },
+            handleResize() {
+                // console.log("window.innerWidth", window.innerWidth)
+                this.windowWidth = window.innerWidth;
             }
-        }
+        },
+        created(){
+            window.addEventListener('resize', this.handleResize);
+            this.handleResize();
+        },
+        mounted(){
+            this.selectedNav = this.NavBarComponent[0].routerName;
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.handleResize);
+        },
     }
 </script>
