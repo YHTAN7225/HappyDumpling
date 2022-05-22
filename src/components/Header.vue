e2d8b05061cafba23d10f7b9c49e1db799f8fc4d
YHTAN7225 <yihow_tan@hotmail.com>
2022-05-22 22:12:08 +0800

    [Header] change responsive login/profile button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,55 +1,5 @@
 <template>
     <div>
-        <!-- <SfHeader
-            :logo="require('@/assets/images/HappyDumplingLogoLong.png')"
-            :logoHeight="50"
-            :logoWidth="150"
-            title="Storefront UI"
-            cartIcon="empty_cart"
-            accountIcon="profile"
-            searchPlaceholder="Search for items"
-            @enter:search="search"
-            @change:search="changeSearch"
-            @click:cart="redirect('cart')"
-            @click:account="redirect('account')"
-        >
-            <template v-slot:aside>
-                <div v-if="showAside" @click="showHeaderMenu = !showHeaderMenu">
-                    <SfIcon
-                        icon="tiles"
-                        size="lg"
-                        color="black"
-                        viewBox="0 0 24 24"
-                        :coverage="1"
-                    />
-                </div>
-            </template>
-            <template v-slot:navigation>
-                <div>
-                    <nav class="sf-breadcrumbs" aria-label="breadcrumb">
-                        <ol class="sf-breadcrumbs__list">
-                            <li :class="'sf-breadcrumbs__list-item' + (selectedNav == item.routerName ? ' text-selected :' : '')" 
-                            aria-current="page" 
-                            v-for="item in NavBarComponent" 
-                            :key="item.name" 
-                            @click="selectedNav = item.routerName; redirect(item.routerName)">
-                                {{item.name}}
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
-            </template>
-        </SfHeader>
-        <SfAccordion open="" :multiple="false" transition="" v-if="showHeaderMenu">
-            <SfList>
-                <SfListItem 
-                :class="(selectedNav == item.routerName) ? 'mobile_header_list_selected' : 'mobile_header_list'"
-                v-for="item in NavBarComponent" 
-                :key="item.name">
-                    <SfMenuItem :label="item.name" @click="selectedNav = item.routerName; redirect(item.routerName); showHeaderMenu=false"/>
-                </SfListItem>
-            </SfList>
-        </SfAccordion> -->
         <div>
             <b-navbar toggleable="lg" type="dark" variant="dark">
                 <b-navbar-brand  @click="$router.push({name:'home'})">
@@ -72,6 +22,18 @@
                         >
                             <div :class="'fontSizeLarger navItem ' + ($route.name == nav.routerName ? 'activeNavItem' : '')">{{nav.name}}</div>
                         </b-nav-item>
+                        <b-nav-item 
+                            v-if="$global.state.user.userToken == ''"
+                            @click="redirect('login')"
+                        >
+                            <div :class="'fontSizeLarger navItem ' + ($route.name == 'login' ? 'activeNavItem' : '')">{{"Login"}}</div>
+                        </b-nav-item>
+                        <b-nav-item 
+                            v-else
+                            @click="redirect('profile')"
+                        >
+                            <div :class="'fontSizeLarger navItem ' + ($route.name == 'profile' ? 'activeNavItem' : '')">{{"Profile"}}</div>
+                        </b-nav-item>
                     </b-navbar-nav>
                 </b-collapse>
             </b-navbar>   
@@ -83,12 +45,6 @@
 </template>
 
 <script>
-import {SfHeader} from "@storefront-ui/vue";
-import {SfAccordion} from "@storefront-ui/vue";
-import {SfIcon} from "@storefront-ui/vue";
-import {SfList} from "@storefront-ui/vue";
-import {SfMenuItem} from "@storefront-ui/vue";
-
     export default {
         name : 'Header',
         data(){
@@ -114,10 +70,6 @@ import {SfMenuItem} from "@storefront-ui/vue";
                         name : "Product",
                         routerName : "product"
                     },
-                    {
-                        name : "Login",
-                        routerName : "login"
-                    },
                 ]
             },
             NavBarTitle : {
@@ -127,11 +79,7 @@ import {SfMenuItem} from "@storefront-ui/vue";
             },
         },
         components : {
-            SfHeader,
-            SfAccordion,
-            SfIcon,
-            SfList,
-            SfMenuItem
+
         },
         watch : {
             windowWidth : function(oldVal){            
